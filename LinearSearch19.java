f6e44786dae2986e54e018eca2d071186dd42a3e
mrfauzii <reishifauzi24@gmail.com>
2023-10-29 22:40:15 +0700

    modifikasi

--- LinearSearch19.java
+++ LinearSearch19.java
@@ -1,17 +1,32 @@
+import java.util.Scanner;
 public class LinearSearch19 {
     public static void main(String[] args) {
-        
-        int [] arrayInt = {34, 18, 26, 48, 72, 20, 56, 63};
-        int key = 20;
-        int hasil = 0;
+        Scanner sc = new Scanner(System.in);
 
-        for (int i = 0; i < arrayInt.length; i++) {
+        System.out.print("Masukkan jumlah elemen array: ");
+        int jumlahElemen = sc.nextInt();
+        int[] arrayInt = new int[jumlahElemen];
+
+        for (int i = 0; i < jumlahElemen; i++) {
+            System.out.print("Masukkan elemen array ke-" + i + " : ");
+            arrayInt[i] = sc.nextInt();
+        }
+
+        System.out.print("Masukkan key yang ingin dicari: ");
+        int key = sc.nextInt();
+        int hasil = -1; // Inisialisasi hasil dengan -1 jika key tidak ditemukan
+
+        for (int i = 0; i < jumlahElemen; i++) {
             if (arrayInt[i] == key) {
                 hasil = i;
                 break;
             }
         }
 
-        System.out.println("Key ada dalam arrat pada posisi indeks ke- "+hasil);
+        if (hasil != -1) {
+            System.out.println("Key ada di posisi indeks ke-" + hasil);
+        } else {
+            System.out.println("Key tidak ditemukan dalam array.");
+        }
     }
 }
