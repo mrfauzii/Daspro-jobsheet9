ca3093b25d55834070494dbedf3df59ec0d1e77a
mrfauzii <reishifauzi24@gmail.com>
2023-10-29 19:24:11 +0700

    modif percobaan 1

--- ArrayBilangan19.java
+++ ArrayBilangan19.java
@@ -4,17 +4,10 @@ public class ArrayBilangan19{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[] bil = new int[4];
-        bil[0] = 5;
-        bil [1] = 13;
-        bil[2] = -7;
-        bil[3] = 17;
+        int[] bil = {5,13,-7,17};
 
-        System.out.println(bil[0]);
-        System.out.println(bil[1]);
-        System.out.println(bil[2]);
-        System.out.println(bil[3]);
-
-
+        for (int i = 0; i <4; i++) {
+            System.out.println(bil[i]);
+        }
     }
 }
